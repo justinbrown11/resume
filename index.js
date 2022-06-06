cbc1012967cc825bfe98506687333f7b628f979c
justinbrown11 <96081581+justinbrown11@users.noreply.github.com>
2022-06-06 16:23:22 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -64,4 +64,4 @@ app.post("/msg", async (req, res) => {
 });
 
 //start app
-app.listen(port, () => console.log('Server is listening on port ' + port));
+app.listen(port, () => console.log('Server is listening'));
